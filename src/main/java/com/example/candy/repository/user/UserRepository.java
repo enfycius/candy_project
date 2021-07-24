98c143849742061d6d27861d25060a2edd0cdaf8
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-07-24 20:59:21 +0900

    Create UserRepository.java

---
+++ src/main/java/com/example/candy/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.candy.repository.user;
+
+import com.example.candy.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+}
