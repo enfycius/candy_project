b00403a6fa01dd50598c92a5b82c51016b94b959
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-07-25 04:41:40 +0900

    Authority value로 얻을 수 있게 수정

--- src/main/java/com/example/candy/domain/user/Authority.java
+++ src/main/java/com/example/candy/domain/user/Authority.java
@@ -1,5 +1,29 @@
 package com.example.candy.domain.user;
 
 public enum Authority {
-    STUDENT, PARENT, ADMIN
+    STUDENT("ROLE_STUDENT"),
+    PARENT("ROLE_PARENT"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+
+    Authority(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static Authority of(String name) {
+        for (Authority role : Authority.values()) {
+            if (role.name().equalsIgnoreCase(name)) {
+                return role;
+            }
+        }
+        return null;
+    }
+
+
 }
+
