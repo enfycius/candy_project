d8624703a77143b384615a023589777b5e021baf
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-07-30 13:22:32 +0900

    Update ChallengeHistory.java
    setter 추가

--- src/main/java/com/example/candy/domain/challenge/ChallengeHistory.java
+++ src/main/java/com/example/candy/domain/challenge/ChallengeHistory.java
@@ -4,6 +4,7 @@ import com.example.candy.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -13,6 +14,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChallengeHistory {
@@ -37,6 +39,5 @@ public class ChallengeHistory {
     private LocalDateTime completeDate;
     private LocalDateTime createDate;
     private LocalDateTime modifiedDate;
-
 
 }
