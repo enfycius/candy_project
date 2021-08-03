f7f2ba47b76c9459ab69653cf8a30f26660a6ebe
LateNightCoding <leehae8@naver.com>
2021-08-04 00:36:42 +0900

    test: 캔디 충전, 조회 테스트 구현

--- src/test/java/com/example/candy/service/candyHistory/CandyHistoryServiceTest.java
+++ src/test/java/com/example/candy/service/candyHistory/CandyHistoryServiceTest.java
@@ -8,6 +8,7 @@ import com.example.candy.service.user.UserService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -52,25 +53,24 @@ class CandyHistoryServiceTest {
     @Order(2)
     @DisplayName("charge_candy")
     void 캔디_충전() {
-        candyHistory_charge = CandyHistory.builder()
-                .createDate(LocalDateTime.now())
-                .eventType(EventType.CHARGE)
-                .user(user)
-                .amount(50)
-                .parentCandy(50)
-                .studentCandy(0)
-                .totalCandy(50)
-                .build();
-        CandyHistory candyHistory = candyHistoryService.save(candyHistory_charge);
-        assertEquals(candyHistory.getTotalCandy(), candyHistory_charge.getTotalCandy());
+        CandyHistory candyHistory = candyHistoryService.chargeCandy(user.getId(), 80);
+        assertEquals(candyHistory.getTotalCandy(), 80);
+        assertEquals(candyHistory.getStudentCandy(), 0);
+        CandyHistory findCandy = candyHistoryService.findLatestOne(user.getId());
+        assertEquals(findCandy.getTotalCandy(), 80);
+
+        CandyHistory candyHistory2 = candyHistoryService.chargeCandy(user.getId(), 50);
+        assertEquals(candyHistory2.getTotalCandy(), 130);
+        assertEquals(candyHistory2.getStudentCandy(), 0);
+        CandyHistory findCandy2 = candyHistoryService.findLatestOne(user.getId());
+        assertEquals(findCandy2.getTotalCandy(), 130);
     }
 
     @Test
     @Order(3)
     @DisplayName("find_candy")
     void 가장_최근_캔디_조회() {
-        Optional<CandyHistory> findCandy = candyHistoryService.findOne(user.getId());
-        CandyHistory candyHistory = findCandy.get();
-        assertEquals(candyHistory.getTotalCandy(), 50);
+        CandyHistory findCandy = candyHistoryService.findLatestOne(user.getId());
+        assertEquals(findCandy.getTotalCandy(), 130);
     }
 }
