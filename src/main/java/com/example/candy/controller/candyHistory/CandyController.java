fefbb9499e14395b474b2b591d99213068ede8bc
LateNightCoding <leehae8@naver.com>
2021-08-04 00:35:39 +0900

    feat: 캔디 컨트롤러 구현

---
+++ src/main/java/com/example/candy/controller/candyHistory/CandyController.java
@@ -0,0 +1,17 @@
+package com.example.candy.controller.candyHistory;
+
+import com.example.candy.controller.ApiResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candy")
+public class CandyController {
+
+//    @PostMapping("/charge")
+//    public ApiResult<CandyResponseDto> chargeCandy(@RequestBody CandyChargeRequestDto candyChargeRequestDto) {
+//        return ApiResult("hi");
+//    }
+}
