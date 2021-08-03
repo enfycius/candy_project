2c561e37d330ef05d01a2ec997ff311d7a6e33da
LateNightCoding <leehae8@naver.com>
2021-08-04 00:36:10 +0900

    feat: 캔디 충전 서비스 로직 구현

--- src/main/java/com/example/candy/service/candyHistory/CandyHistoryService.java
+++ src/main/java/com/example/candy/service/candyHistory/CandyHistoryService.java
@@ -4,17 +4,21 @@ import com.example.candy.domain.candy.CandyHistory;
 import com.example.candy.domain.candy.EventType;
 import com.example.candy.domain.user.User;
 import com.example.candy.repository.candy.CandyHistoryRepository;
+import com.example.candy.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class CandyHistoryService {
 
     @Autowired
     private CandyHistoryRepository candyHistoryRepository;
+    @Autowired
+    private UserService userService;
 
     @Transactional
     public CandyHistory initCandy(User user) {
@@ -24,12 +28,33 @@ public class CandyHistoryService {
                 .parentCandy(0)
                 .studentCandy(0)
                 .amount(0)
+                .user(user)
                 .createDate(LocalDateTime.now())
                 .build();
         CandyHistory savedCandyHistory = save(candyHistory);
         savedCandyHistory.addUser(user);
         return savedCandyHistory;
     }
+
+    @Transactional
+    public CandyHistory chargeCandy(Long userId, int amount) {
+        User user = userService.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("no such userId"));
+
+        CandyHistory latestCandy = findLatestOne(userId);
+        CandyHistory candyHistory = CandyHistory.builder()
+                .totalCandy(latestCandy.getTotalCandy() + amount)
+                .eventType(EventType.CHARGE)
+                .parentCandy(latestCandy.getParentCandy() + amount)
+                .studentCandy(latestCandy.getStudentCandy())
+                .amount(amount)
+                .createDate(LocalDateTime.now())
+                .user(user)
+                .build();
+        return save(candyHistory);
+    }
+
+    public CandyHistory findLatestOne(Long userId) { return candyHistoryRepository.findTopByUser_IdOrderByCreateDateDesc(userId); }
 
     public CandyHistory save(CandyHistory candyHistory) {
         return candyHistoryRepository.save(candyHistory);
