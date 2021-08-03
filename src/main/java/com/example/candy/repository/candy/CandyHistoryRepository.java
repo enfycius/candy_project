4a0f9d9ca9cacff421ccd86a1ade788de92cd034
LateNightCoding <leehae8@naver.com>
2021-08-04 00:35:54 +0900

    refactor: Optional 해제

--- src/main/java/com/example/candy/repository/candy/CandyHistoryRepository.java
+++ src/main/java/com/example/candy/repository/candy/CandyHistoryRepository.java
@@ -1,13 +1,8 @@
 package com.example.candy.repository.candy;
 
 import com.example.candy.domain.candy.CandyHistory;
-import com.example.candy.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 public interface CandyHistoryRepository extends JpaRepository<CandyHistory, Long> {
-    Optional<CandyHistory> findTopByUser_IdOrderByCreateDateDesc(Long id);
+    CandyHistory findTopByUser_IdOrderByCreateDateDesc(Long id);
 }
