0476f0968a5066677e7ba6865fd4938ab87136b8
LateNightCoding <leehae8@naver.com>
2021-08-03 23:41:44 +0900

    feat: 가장 최근 candy 내역 조회 메소드 구현

--- src/main/java/com/example/candy/repository/candy/CandyHistoryRepository.java
+++ src/main/java/com/example/candy/repository/candy/CandyHistoryRepository.java
@@ -1,7 +1,13 @@
 package com.example.candy.repository.candy;
 
 import com.example.candy.domain.candy.CandyHistory;
+import com.example.candy.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface CandyHistoryRepository extends JpaRepository<CandyHistory, Long> {
+    Optional<CandyHistory> findTopByUser_IdOrderByCreateDateDesc(Long id);
 }
